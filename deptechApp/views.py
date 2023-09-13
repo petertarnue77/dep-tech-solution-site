aa39a42e58e7f4f31a08e882259f46fe71438503
Peter K Tarnue <petertarnue@gmail.com>
2023-09-13 16:15:21 +0000

    another update
    Another update

--- deptechApp/views.py
+++ deptechApp/views.py
@@ -5,3 +5,19 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, "index.html")
+
+
+def business(request):
+    return render(request, "for_business.html")
+
+
+def talents(request):
+    return render(request, "talents.html")
+
+
+def our_solution(request):
+    return render(request, "our_solutions.html")
+
+
+def why_we_exist(request):
+    return render(request, "why_we_exist.html")
